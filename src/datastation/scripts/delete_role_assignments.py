ec8690915f9815dd2af73041d8d233bad21c5f2c
lindareijnhoudt <linda.reijnhoudt@dans.knaw.nl>
2022-08-29 17:03:36 +0300

    Let dv-datasets-remove-role-assignment give the specified output

--- src/datastation/scripts/delete_role_assignments.py
+++ src/datastation/scripts/delete_role_assignments.py
@@ -1,5 +1,6 @@
 import argparse
-import os
+import datetime
+import logging
 
 from datastation.batch_processing import batch_process
 from datastation.config import init
@@ -7,51 +8,56 @@ from datastation.ds_pidsfile import load_pids
 from datastation.dv_api import delete_dataset_role_assignment, get_dataset_roleassigments
 
 
-def delete_roleassignment_action(server_url, api_token, pid, role_assignee, role_alias):
-    deleted_role = False
-    resp_data = get_dataset_roleassigments(server_url, api_token, pid)
-    # print(json.dumps(resp_data, indent=2))
-    for role_assignment in resp_data:
-        assignee = role_assignment['assignee']
-        # role_id = role_assignment['roleId']
-        alias = role_assignment['_roleAlias']
-        print("Role assignee: " + assignee + ', role alias: ' + alias)
-        if assignee == role_assignee and alias == role_alias:
-            # delete this one
-            assignment_id = role_assignment['id']
-            print("Try deleting the role assignment")
-            delete_dataset_role_assignment(server_url, api_token, pid, assignment_id)
-            print("Done")
-            deleted_role = True
+def delete_roleassignment_action(server_url, api_token, pid, role_assignee, role_alias, dry_run):
+    current_assignments = get_dataset_roleassigments(server_url, api_token, pid)
+    found = False
+    assignment_id = None
+    for current_assignment in current_assignments:
+        if current_assignment.get('assignee') == role_assignee and current_assignment.get('_roleAlias') == role_alias:
+            assignment_id = current_assignment.get('id')
+            found = True
+            break
+
+    action = "Removed" if found else "None"
+    print("{},{},{},{},{}".format(pid, datetime.datetime.now(), role_assignee, role_alias, action))
+
+    if dry_run:
+        if found:
+            logging.info("Dry run: not removing {} as {} for dataset {}".format(role_assignee, role_alias, pid))
         else:
-            print("Leave as-is")
-    return deleted_role
+            logging.info("Dry run: {} is not a {} for dataset {}".format(role_assignee, role_alias, pid))
+    else:
+        if found:
+            delete_dataset_role_assignment(server_url, api_token, pid, assignment_id)
+        else:
+            logging.warning("{} is not a {} for dataset {}".format(role_assignee, role_alias, pid))
 
 
-def delete_roleassignments_command(config, pids_file, role_assignee, role_alias):
-    # look for inputfile in configured OUTPUT_DIR
-    full_name = os.path.join(config['files']['output_dir'], pids_file)
-    pids = load_pids(full_name)
+def delete_roleassignments_command(server_url, api_token, pids_file, role_assignee, role_alias, dry_run):
+    pids = load_pids(pids_file)
 
-    # could be fast, but depends on number of files inside the dataset
-    batch_process(pids, lambda pid: delete_roleassignment_action(config['dataverse']['server_url'],
-                                                                 config.DATAVERSE_API_TOKEN, pid, role_assignee,
-                                                                 role_alias), config['files']['output_dir'], delay=1.5)
+    print("DOI,Modified,Assignee,Role,Change")
+    batch_process(pids, lambda pid: delete_roleassignment_action(server_url,api_token, pid, role_assignee,
+                                                                 role_alias, dry_run), None, delay=1.5)
 
 
 def main():
     config = init()
     parser = argparse.ArgumentParser(
         description='Delete role assigment for datasets with the pids in the given inputfile')
-    parser.add_argument("role_assignee", help="Role assignee (example: @dataverseAdmin)")
-    parser.add_argument("role_alias", help="Role alias (example: contributor")
-    parser.add_argument('-p', '--pids_file', default='dataset_pids.txt', help='The input file with the dataset pids')
+    parser.add_argument('--dry-run', dest='dry_run', help="only logs the actions, nothing is executed", action='store_true')
+    parser.add_argument('-r', "--role_assignment", help="Role assignee and alias (example: @dataverseAdmin=contributor)")
+    parser.add_argument('-d', '--datasets', dest="pids_file",
+                        default='dataset_pids.txt', help='The input file with the dataset pids')
     args = parser.parse_args()
 
-    role_assignee = args.role_assignee
-    role_alias = args.role_alias
+    role_assignee = args.role_assignment.split('=')[0]
+    role_alias = args.role_assignment.split('=')[1]
 
-    delete_roleassignments_command(config, args.pids_file, role_assignee, role_alias)
+    server_url = config['dataverse']['server_url']
+    api_token = config['dataverse']['api_token']
+
+    delete_roleassignments_command(server_url, api_token, args.pids_file, role_assignee, role_alias, args.dry_run)
 
 
 if __name__ == '__main__':
