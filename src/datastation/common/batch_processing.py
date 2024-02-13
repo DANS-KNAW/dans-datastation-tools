ebcad95d5163f1aa3017f3b17614d06f18d3e365
paulboon <paul.boon@dans.knaw.nl>
2024-02-13 11:32:02 +0100

    Refactoring of the BatchProcessor classes

--- src/datastation/common/batch_processing.py
+++ src/datastation/common/batch_processing.py
@@ -3,6 +3,52 @@ import os
 import time
 
 from datastation.common.csv import CsvReport
+from datastation.common.utils import plural
+
+
+# Base class for batch processing of items
+class BatchProcessor:
+    def __init__(self, item_name="item", wait=0.1, fail_on_first_error=True):
+        self.item_name = item_name
+        self.wait = wait
+        self.fail_on_first_error = fail_on_first_error
+
+    def process_items(self, items, callback):
+        if type(items) is list:
+            num_items = len(items)
+            logging.info(f"Start batch processing on {num_items} {plural(self.item_name)}")
+        else:
+            logging.info(f"Start batch processing on unknown number of {plural(self.item_name)}")
+            num_items = -1
+        i = 0
+        for item in items:
+            i += 1
+            try:
+                if self.wait > 0 and i > 1:
+                    logging.debug(f"Waiting {self.wait} seconds before processing next {self.item_name}")
+                    time.sleep(self.wait)
+                logging.info(f"Processing {i} of {num_items}: {item}")
+                callback(item)
+            except Exception as e:
+                logging.exception("Exception occurred", exc_info=True)
+                if self.fail_on_first_error:
+                    logging.error(f"Stop processing because of an exception: {e}")
+                    break
+                logging.debug("fail_on_first_error is False, continuing...")
+
+
+def get_provided_items_iterator(item_or_items_file, item_name="item"):
+    if item_or_items_file is None:
+        logging.debug(f"No {plural(item_name)} provided.")
+        return None
+    elif os.path.isfile(os.path.expanduser(item_or_items_file)):
+        items = []
+        with open(os.path.expanduser(item_or_items_file)) as f:
+            for line in f:
+                items.append(line.strip())
+        return items
+    else:
+        return [item_or_items_file]
 
 
 def get_pids(pid_or_pids_file, search_api=None, query="*", subtree="root", object_type="dataset", dry_run=False):
@@ -24,14 +70,8 @@ def get_pids(pid_or_pids_file, search_api=None, query="*", subtree="root", objec
     if pid_or_pids_file is None:
         result = search_api.search(query=query, subtree=subtree, object_type=object_type, dry_run=dry_run)
         return map(lambda rec: rec['global_id'], result)
-    elif os.path.isfile(os.path.expanduser(pid_or_pids_file)):
-        pids = []
-        with open(os.path.expanduser(pid_or_pids_file)) as f:
-            for line in f:
-                pids.append(line.strip())
-        return pids
     else:
-        return [pid_or_pids_file]
+        return get_provided_items_iterator(pid_or_pids_file, "pid")
 
 
 def get_aliases(alias_or_aliases_file, dry_run=False):
@@ -49,43 +89,17 @@ def get_aliases(alias_or_aliases_file, dry_run=False):
         # from the tree, but this is not implemented yet.
         logging.warning(f"No aliases provided, nothing to do.")
         return None
-    elif os.path.isfile(os.path.expanduser(alias_or_aliases_file)):
-        aliases = []
-        with open(os.path.expanduser(alias_or_aliases_file)) as f:
-            for line in f:
-                aliases.append(line.strip())
-        return aliases
     else:
-        return [alias_or_aliases_file]
+        return get_provided_items_iterator(alias_or_aliases_file, "alias")
 
 
-class DatasetBatchProcessor:
+class DatasetBatchProcessor(BatchProcessor):
+
     def __init__(self, wait=0.1, fail_on_first_error=True):
-        self.wait = wait
-        self.fail_on_first_error = fail_on_first_error
+        super().__init__("pid", wait, fail_on_first_error)
 
     def process_pids(self, pids, callback):
-        if type(pids) is list:
-            num_pids = len(pids)
-            logging.info(f"Start batch processing on {num_pids} pids")
-        else:
-            logging.info(f"Start batch processing on unknown number of pids")
-            num_pids = -1
-        i = 0
-        for pid in pids:
-            i += 1
-            try:
-                if self.wait > 0 and i > 1:
-                    logging.debug(f"Waiting {self.wait} seconds before processing next pid")
-                    time.sleep(self.wait)
-                logging.info(f"Processing {i} of {num_pids}: {pid}")
-                callback(pid)
-            except Exception as e:
-                logging.exception("Exception occurred", exc_info=True)
-                if self.fail_on_first_error:
-                    logging.error(f"Stop processing because of an exception: {e}")
-                    break
-                logging.debug("fail_on_first_error is False, continuing...")
+        super().process_items(pids, callback)
 
 
 class DatasetBatchProcessorWithReport(DatasetBatchProcessor):
@@ -102,36 +116,13 @@ class DatasetBatchProcessorWithReport(DatasetBatchProcessor):
             super().process_pids(pids, lambda pid: callback(pid, csv_report))
 
 
-class DataverseBatchProcessor:
-    """
-    A batch processor for dataverses (collections) using their alias as unique identifier.
-    """
+class DataverseBatchProcessor(BatchProcessor):
+
     def __init__(self, wait=0.1, fail_on_first_error=True):
-        self.wait = wait
-        self.fail_on_first_error = fail_on_first_error
+        super().__init__("alias", wait, fail_on_first_error)
 
     def process_aliases(self, aliases, callback):
-        if type(aliases) is list:
-            num_aliases = len(aliases)
-            logging.info(f"Start batch processing on {num_aliases} dataverse aliases")
-        else:
-            logging.info(f"Start batch processing on unknown number of dataverse aliases")
-            num_aliases = -1
-        i = 0
-        for alias in aliases:
-            i += 1
-            try:
-                if self.wait > 0 and i > 1:
-                    logging.debug(f"Waiting {self.wait} seconds before processing next dataverse alias")
-                    time.sleep(self.wait)
-                logging.info(f"Processing {i} of {num_aliases}: {alias}")
-                callback(alias)
-            except Exception as e:
-                logging.exception("Exception occurred", exc_info=True)
-                if self.fail_on_first_error:
-                    logging.error(f"Stop processing because of an exception: {e}")
-                    break
-                logging.debug("fail_on_first_error is False, continuing...")
+        super().process_items(aliases, callback)
 
 
 class DataverseBatchProcessorWithReport(DataverseBatchProcessor):
