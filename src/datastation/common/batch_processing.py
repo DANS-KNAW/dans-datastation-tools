8d4a65c4b6da1036864fd0860710561727937c82
paulboon <paul.boon@dans.knaw.nl>
2024-02-13 10:21:20 +0100

    Completed functionality of dv-dataverse-role-assignment

--- src/datastation/common/batch_processing.py
+++ src/datastation/common/batch_processing.py
@@ -34,6 +34,31 @@ def get_pids(pid_or_pids_file, search_api=None, query="*", subtree="root", objec
         return [pid_or_pids_file]
 
 
+def get_aliases(alias_or_aliases_file, dry_run=False):
+    """
+
+    Args:
+        alias_or_aliases_file: The dataverse alias, or a file with a list of aliases.
+        dry_run:          Do not perform the action, but show what would be done.
+                          Only applicable if pid_or_pids_file is None.
+
+    Returns: an iterator with aliases
+    """
+    if alias_or_aliases_file is None:
+        # The tree of all (published) dataverses could be retrieved and aliases could recursively be extracted
+        # from the tree, but this is not implemented yet.
+        logging.warning(f"No aliases provided, nothing to do.")
+        return None
+    elif os.path.isfile(os.path.expanduser(alias_or_aliases_file)):
+        aliases = []
+        with open(os.path.expanduser(alias_or_aliases_file)) as f:
+            for line in f:
+                aliases.append(line.strip())
+        return aliases
+    else:
+        return [alias_or_aliases_file]
+
+
 class BatchProcessor:
     def __init__(self, wait=0.1, fail_on_first_error=True):
         self.wait = wait
@@ -75,3 +100,49 @@ class BatchProcessorWithReport(BatchProcessor):
     def process_pids(self, pids, callback):
         with CsvReport(os.path.expanduser(self.report_file), self.headers) as csv_report:
             super().process_pids(pids, lambda pid: callback(pid, csv_report))
+
+
+class DataverseBatchProcessor:
+    """
+    A batch processor for dataverses (collections) using their alias as unique identifier.
+    """
+    def __init__(self, wait=0.1, fail_on_first_error=True):
+        self.wait = wait
+        self.fail_on_first_error = fail_on_first_error
+
+    def process_aliases(self, aliases, callback):
+        if type(aliases) is list:
+            num_aliases = len(aliases)
+            logging.info(f"Start batch processing on {num_aliases} dataverse aliases")
+        else:
+            logging.info(f"Start batch processing on unknown number of dataverse aliases")
+            num_aliases = -1
+        i = 0
+        for alias in aliases:
+            i += 1
+            try:
+                if self.wait > 0 and i > 1:
+                    logging.debug(f"Waiting {self.wait} seconds before processing next dataverse alias")
+                    time.sleep(self.wait)
+                logging.info(f"Processing {i} of {num_aliases}: {alias}")
+                callback(alias)
+            except Exception as e:
+                logging.exception("Exception occurred", exc_info=True)
+                if self.fail_on_first_error:
+                    logging.error(f"Stop processing because of an exception: {e}")
+                    break
+                logging.debug("fail_on_first_error is False, continuing...")
+
+
+class DataverseBatchProcessorWithReport(DataverseBatchProcessor):
+
+    def __init__(self, report_file=None, headers=None, wait=0.1, fail_on_first_error=True):
+        super().__init__(wait, fail_on_first_error)
+        if headers is None:
+            headers = ["alias", "Modified", "Change"]
+        self.report_file = report_file
+        self.headers = headers
+
+    def process_aliases(self, aliases, callback):
+        with CsvReport(os.path.expanduser(self.report_file), self.headers) as csv_report:
+            super().process_aliases(aliases, lambda alias: callback(alias, csv_report))
--- src/datastation/dv_dataverse_role_assignment.py
+++ src/datastation/dv_dataverse_role_assignment.py
@@ -3,23 +3,23 @@ from datetime import datetime
 
 import rich
 
-from datastation.common.batch_processing import get_pids, BatchProcessorWithReport
+from datastation.common.batch_processing import DataverseBatchProcessorWithReport, get_aliases
 from datastation.common.config import init
 from datastation.common.utils import add_batch_processor_args, add_dry_run_arg
-from datastation.dataverse.dataset_api import DatasetApi
 from datastation.dataverse.dataverse_api import DataverseApi
 from datastation.dataverse.dataverse_client import DataverseClient
 
 
 def add_role_assignments(args, dataverse_client: DataverseClient):
-    pids = get_pids(args.pid_or_pid_file)
-    batch_processor = BatchProcessorWithReport(wait=args.wait, fail_on_first_error=args.fail_fast,
-                                               report_file=args.report_file,
-                                               headers=['DOI', 'Modified', 'Assignee', 'Role', 'Change'])
-    batch_processor.process_pids(pids,
-                                 lambda pid, csv_report: add_role_assignment(args.role_assignment,
-                                                                             dataverse_api=dataverse_client.dataverse(pid),
-                                                                             csv_report=csv_report))
+    aliases = get_aliases(args.alias_or_alias_file)
+    batch_processor = DataverseBatchProcessorWithReport(wait=args.wait, fail_on_first_error=args.fail_fast,
+                                                        report_file=args.report_file,
+                                                        headers=['alias', 'Modified', 'Assignee', 'Role', 'Change'])
+    batch_processor.process_aliases(aliases,
+                                    lambda alias,
+                                    csv_report: add_role_assignment(args.role_assignment,
+                                                                    dataverse_api=dataverse_client.dataverse(alias),
+                                                                    csv_report=csv_report))
 
 
 def add_role_assignment(role_assignment, dataverse_api: DataverseApi, csv_report, dry_run: bool = False):
@@ -34,7 +34,7 @@ def add_role_assignment(role_assignment, dataverse_api: DataverseApi, csv_report
         dataverse_api.add_role_assignment(assignee, role, dry_run=dry_run)
         action = "Added"
     csv_report.write(
-        {'DOI': dataverse_api.get_alias(), 'Modified': datetime.now(), 'Assignee': assignee, 'Role': role,
+        {'alias': dataverse_api.get_alias(), 'Modified': datetime.now(), 'Assignee': assignee, 'Role': role,
          'Change': action})
 
 
@@ -50,20 +50,21 @@ def in_current_assignments(assignee, role, dataverse_api: DataverseApi):
 
 
 def list_role_assignments(args, dataverse_client):
-    r = dataverse_client.dataverse(args.pid).get_role_assignments()
+    r = dataverse_client.dataverse(args.alias).get_role_assignments()
     if r is not None:
         rich.print_json(data=r)
 
 
 def remove_role_assignments(args, dataverse_client: DataverseClient):
-    pids = get_pids(args.pid_or_pid_file)
-    batch_processor = BatchProcessorWithReport(wait=args.wait, report_file=args.report_file,
-                                               headers=['alias', 'Modified', 'Assignee', 'Role', 'Change'])
-    batch_processor.process_pids(pids,
-                                 lambda pid,
-                                 csv_report: remove_role_assignment(args.role_assignment,
-                                                                    dataverse_api=dataverse_client.dataverse(pid),
-                                                                    csv_report=csv_report))
+    aliases = get_aliases(args.alias_or_alias_file)
+    batch_processor = DataverseBatchProcessorWithReport(wait=args.wait, report_file=args.report_file,
+                                                        headers=['alias', 'Modified', 'Assignee', 'Role', 'Change'])
+    batch_processor.process_aliases(aliases,
+                                    lambda alias,
+                                    csv_report: remove_role_assignment(args.role_assignment,
+                                                                        dataverse_api=dataverse_client.dataverse(alias),
+                                                                        csv_report=csv_report))
+
 
 def remove_role_assignment(role_assignment, dataverse_api: DataverseApi, csv_report, dry_run: bool = False):
     assignee = role_assignment.split('=')[0]
@@ -78,7 +79,7 @@ def remove_role_assignment(role_assignment, dataverse_api: DataverseApi, csv_rep
                 action = "Removed"
                 break
     else:
-        print("{} is not {} for dataset {}".format(assignee, role, dataverse_api.get_alias()))
+        print("{} is not {} for dataverse {}".format(assignee, role, dataverse_api.get_alias()))
     csv_report.write(
         {'alias': dataverse_api.get_alias(), 'Modified': datetime.now(), 'Assignee': assignee, 'Role': role,
          'Change': action})
@@ -87,7 +88,7 @@ def remove_role_assignment(role_assignment, dataverse_api: DataverseApi, csv_rep
 def main():
     config = init()
     dataverse_client = DataverseClient(config['dataverse'])
-    batch_processor = BatchProcessorWithReport(headers=['alias', 'Modified', 'Assignee', 'Role', 'Change'])
+    batch_processor = DataverseBatchProcessorWithReport(headers=['alias', 'Modified', 'Assignee', 'Role', 'Change'])
 
     # Create main parser and subparsers
     parser = argparse.ArgumentParser(description='Manage role assignments on one or more datasets.')
@@ -97,7 +98,8 @@ def main():
     parser_add = subparsers.add_parser('add', help='add role assignment to specified dataset(s)')
     parser_add.add_argument('role_assignment',
                             help='role assignee and alias (example: @dataverseAdmin=contributor) to add')
-    parser_add.add_argument('pid_or_pid_file', help='the dataset pid or the input file with the dataset pids')
+    parser_add.add_argument('alias_or_alias_file',
+                            help='The dataverse alias or the input file with the dataverse aliases')
     add_batch_processor_args(parser_add)
     add_dry_run_arg(parser_add)
 
@@ -107,15 +109,16 @@ def main():
     parser_remove = subparsers.add_parser('remove', help='remove role assignment from specified dataset(s)')
     parser_remove.add_argument('role_assignment',
                                help='role assignee and alias (example: @dataverseAdmin=contributor)')
-    parser_remove.add_argument('pid_or_pid_file', help='The dataset pid or the input file with the dataset pids')
+    parser_remove.add_argument('alias_or_alias_file',
+                               help='The dataverse alias or the input file with the dataverse aliases')
     add_batch_processor_args(parser_remove)
     add_dry_run_arg(parser_remove)
     parser_remove.set_defaults(func=lambda _: remove_role_assignments(_, dataverse_client))
 
     # List role assignments
     parser_list = subparsers.add_parser('list',
-                                        help='list role assignments for specified dataset (only one pid allowed)')
-    parser_list.add_argument('pid', help='the dataset pid')
+                                        help='list role assignments for specified dataverse (only one alias allowed)')
+    parser_list.add_argument('alias', help='the dataverse alias')
     add_dry_run_arg(parser_list)
     parser_list.set_defaults(func=lambda _: list_role_assignments(_, dataverse_client))
 
