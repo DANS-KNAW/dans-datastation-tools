1d4805982600e095ad69305e0a11da41623533e8
paulboon <paul.boon@dans.knaw.nl>
2024-02-14 13:32:57 +0100

    Refactoring; removed alias as input parameter for all members of the dataverse_api

--- src/datastation/dataverse/dataverse_api.py
+++ src/datastation/dataverse/dataverse_api.py
@@ -6,7 +6,7 @@ from datastation.common.utils import print_dry_run_message
 
 
 class DataverseApi:
-    def __init__(self, server_url, api_token, alias=None):
+    def __init__(self, server_url, api_token, alias):
         self.server_url = server_url
         self.api_token = api_token
         self.alias = alias  # Methods should use this one if specified
@@ -15,9 +15,9 @@ class DataverseApi:
         return self.alias
 
     # get json data for a specific dataverses API endpoint using an API token
-    def get_resource_data(self, resource, alias="root", dry_run=False):
+    def get_resource_data(self, resource, dry_run=False):
         headers = {"X-Dataverse-key": self.api_token}
-        url = f"{self.server_url}/api/dataverses/{alias}/{resource}"
+        url = f"{self.server_url}/api/dataverses/{self.alias}/{resource}"
 
         if dry_run:
             print_dry_run_message(method="GET", url=url, headers=headers)
@@ -29,21 +29,21 @@ class DataverseApi:
         resp_data = dv_resp.json()["data"]
         return resp_data
 
-    def get_contents(self, alias="root", dry_run=False):
-        return self.get_resource_data("contents", alias, dry_run)
+    def get_contents(self, dry_run=False):
+        return self.get_resource_data("contents", dry_run)
 
-    def get_roles(self, alias="root", dry_run=False):
-        return self.get_resource_data("roles", alias, dry_run)
+    def get_roles(self, dry_run=False):
+        return self.get_resource_data("roles", dry_run)
 
     def get_role_assignments(self, dry_run=False):
-        return self.get_resource_data("assignments", self.alias, dry_run)
+        return self.get_resource_data("assignments", dry_run)
 
-    def get_groups(self, alias="root", dry_run=False):
-        return self.get_resource_data("groups", alias, dry_run)
+    def get_groups(self, dry_run=False):
+        return self.get_resource_data("groups", dry_run)
 
-    def get_storage_size(self, alias="root", dry_run=False):
+    def get_storage_size(self, dry_run=False):
         """ Get dataverse storage size (bytes). """
-        url = f'{self.server_url}/api/dataverses/{alias}/storagesize'
+        url = f'{self.server_url}/api/dataverses/{self.alias}/storagesize'
         headers = {'X-Dataverse-key': self.api_token}
         if dry_run:
             print_dry_run_message(method='GET', url=url, headers=headers)
