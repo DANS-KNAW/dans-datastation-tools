887b4e38ceb8be8b25b99f986b60a755d18f25a9
Ali Sheikhi <aliassheikh@users.noreply.github.com>
2025-02-05 17:22:55 +0100

    DD-1763 Voeg --age parameter toe aan deposit-clean-data

--- src/datastation/deposit_clean_data.py
+++ src/datastation/deposit_clean_data.py
@@ -1,4 +1,6 @@
 import argparse
+from datetime import date, timedelta
+
 from datastation.managedeposit.manage_deposit import ManageDeposit
 from datastation.common.config import init
 
@@ -12,11 +14,18 @@ def clean_manage_deposit_data(server_url, args):
 def main():
     config = init()
     parser = argparse.ArgumentParser(prog='deposit_data_cleaner', description='Clean up dd-manage-deposit database')
-    parser.add_argument('-e', '--enddate', dest='enddate', help='Filter until the record creation of this date')
     parser.add_argument('-s', '--startdate', dest='startdate', help='Filter from the record creation of this date')
-    parser.add_argument('-t', '--state', dest='state', help='The state of the deposit')
-    parser.add_argument('-u', '--user', dest='user', help='The depositor name')
+
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('-a', '--age', dest='age', type=int, help='Filter from record creation older than a number of days before today')
+    group.add_argument('-e', '--enddate', dest='enddate', help='Filter until the record creation of this date')
+
+    parser.add_argument('-t', '--state', dest='state', help='The state of the deposit (repeatable)', action='append')
+    parser.add_argument('-u', '--user', dest='user', help='The depositor name (repeatable)', action='append')
     args = parser.parse_args()
+
+    if args.age is not None: # Note: args is a Namespace object
+        vars(args)['enddate'] = (date.today() + timedelta(days=-args.age)).strftime('%Y-%m-%d')
 
     server_url = config['manage_deposit']['service_baseurl'] + '/delete-deposit'
 
